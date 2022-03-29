16bf955597a933d1db767f423370b47907bbc652
Sean <seanminches@gmail.com>
2022-03-29 17:54:40 -0400

    debugging server routes

---
+++ controllers/userController.js
@@ -0,0 +1,136 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.UserId })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that user ID" })
+          : res.json({
+              user,
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  //update user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.UserId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that user ID" })
+          : res.json({
+              user,
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // delete a user
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.UserId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No such user exists" })
+          : Thought.findOneAndUpdate(
+              { users: req.params.UserId },
+              { $pull: { users: req.params.UserId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: "User deleted, but no thoughts found",
+            })
+          : res.json({ message: "User successfully deleted" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add thought
+  addThought(req, res) {
+    console.log("You are adding a thought");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.UserId },
+      { $addToSet: { thoughts: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Add friend
+  addFriend(req, res) {
+    console.log("You are adding a friend");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.UserId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // remove friend
+  removeFriend(req, res) {
+    console.log("You are removing a friend");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.UserId },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,7 +8,7 @@ const {
   deleteThought,
   createReaction,
   deleteReaction,
-} = require("../../controllers/thoughtController.js");
+} = require("../../controllers/thoughtsController.js");
 
 // /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
